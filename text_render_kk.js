b19651007b81016f6bc6a3869eaeebb21c734d47
iigmir <roc120j@gmail.com>
2019-05-05 17:54:20 +0800

    tablise

--- text_render_kk.js
+++ text_render_kk.js
@@ -7,13 +7,14 @@ module.exports = function({ url, body })
     let dom = new JSDOM( body );
     let prargraphs = [ ...dom.window.document.querySelectorAll("div.entry p") ];
     let texts = [];
-    // console.log( prargraphs );
+    let render_target = {
+        url,
+        title: dom.window.document.querySelector("h1.p-name").textContent,
+        date: dom.window.document.querySelector("p.post-meta a.updated").textContent,
+        author: dom.window.document.querySelector("p.post-meta em").textContent,
+        texts
+    };
     prargraphs.map( p => texts.push( p.textContent ) );
-    output_json.url = url;
-    output_json.title = dom.window.document.querySelector("h1.p-name").textContent;
-    output_json.date = dom.window.document.querySelector("p.post-meta a.updated").textContent;
-    output_json.author = dom.window.document.querySelector("p.post-meta em").textContent;
-    output_json.posts = texts;
-
+    Object.keys( render_target ).map( prop => output_json[prop] = render_target[prop] );
     return JSON.stringify(output_json);
 }
