373349f62e793da2692e05a35651280c4e49bcc8
iigmir <roc120j@gmail.com>
2019-05-06 01:51:33 +0800

    Refactor, add image link

--- text_render_kk.js
+++ text_render_kk.js
@@ -3,24 +3,21 @@ const { JSDOM } = jsdom;
 
 module.exports = function({ url, body })
 {
-    let texts = [];
     let output_json = {};
     let dom = new JSDOM( body );
-    let link_src = [ ...dom.window.document.querySelectorAll(".loop-title a") ];
-    let prargraphs = [ ...dom.window.document.querySelectorAll("div.entry p") ];
-    let links = link_src.map( item => { return { href: item.href, text: item.textContent }; });
+    let document = dom.window.document;
+    let texts  = [ ...document.querySelectorAll("div.entry p") ].map( p => p.textContent );
+    let links  = [ ...document.querySelectorAll(".loop-title a") ].map( item => { return { href: item.href, text: item.textContent }; });
+    let images = [ ...document.querySelectorAll("img[data-src]") ].map( d => d.dataset.src );
     let render_target = {
         url,
+        title:  document.querySelector("h1.p-name").textContent,
+        author: document.querySelector("p.post-meta em").textContent,
+        date:   document.querySelector("p.post-meta a.updated").textContent,
         texts,
         links,
-        title: dom.window.document.querySelector("h1.p-name").textContent,
-        author: dom.window.document.querySelector("p.post-meta em").textContent,
-        date: dom.window.document.querySelector("p.post-meta a.updated").textContent,
+        images
     };
-    
-    
-    prargraphs.map( p => texts.push( p.textContent ) );
     Object.keys( render_target ).map( prop => output_json[prop] = render_target[prop] );
-
     return JSON.stringify(output_json);
 }
