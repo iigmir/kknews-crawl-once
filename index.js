38ed1d6e434026f7de5363be08134b68151405b5
iigmir <roc120j@gmail.com>
2019-05-05 17:51:07 +0800

    Add url

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const text_render = require("./text_render_kk");
 const name_path = require("./name_path");
 // Outgoing module
 const request = require("request");
-const jsdom = require("jsdom");
 const fs = require("fs");
 // Vars
-const { JSDOM } = jsdom;
 let input_urls = process.argv.slice(2);
 
 if( input_urls.length < 1 ) { throw Error("Error: No arguments inputed."); }
@@ -17,7 +15,6 @@ function request_parse(error, response, body, url)
 {
     function handle_files(err)
     {
-        debugger;
         if(err) { return console.log(err); }
         console.log("The file was saved!");
         return;
@@ -25,7 +22,7 @@ function request_parse(error, response, body, url)
     
     if( response && response.statusCode === 200 )
     {
-        fs.writeFile( name_path(url), text_render(body), (err) => handle_files(err) );
+        fs.writeFile( name_path(url), text_render({ url, body }), (err) => handle_files(err) );
     }
     else
     {
--- text_render_kk.js
+++ text_render_kk.js
@@ -1,7 +1,7 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-module.exports = function(body)
+module.exports = function({ url, body })
 {
     let output_json = {};
     let dom = new JSDOM( body );
@@ -9,6 +9,7 @@ module.exports = function(body)
     let texts = [];
     // console.log( prargraphs );
     prargraphs.map( p => texts.push( p.textContent ) );
+    output_json.url = url;
     output_json.title = dom.window.document.querySelector("h1.p-name").textContent;
     output_json.date = dom.window.document.querySelector("p.post-meta a.updated").textContent;
     output_json.author = dom.window.document.querySelector("p.post-meta em").textContent;
